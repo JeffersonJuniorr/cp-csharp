d317e4961408ba7ea2af452fc7bda7c3cb0cc461
Jefferson <labsfiap@fiap.com.br>
2026-05-05 22:07:07 -0300

    structure create

--- ProjectBank/obj/Debug/net8.0/ProjectBank.AssemblyInfo.cs
+++ ProjectBank/obj/Debug/net8.0/ProjectBank.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1dfc86750ca336e19ec5a6a934ba51f3ecbdcb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectBank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
